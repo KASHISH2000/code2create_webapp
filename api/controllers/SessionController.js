f773e0f75941f6859a0dc798f2ea83802cc237ef
hkedia321 <harshit.kedia2015@vit.ac.in>
2017-04-08 18:20:27 +0530

    login fixed

--- api/controllers/SessionController.js
+++ api/controllers/SessionController.js
@@ -6,100 +6,101 @@ module.exports = {
 
   'welcome': function (req, res) {
     res.view({
-         title: "Welcome | Code2Create"
+      title: "Welcome | Code2Create"
     });
     return;
   },
 
   'new': function (req, res) {
-   if(req.session.authenticated)
-   {
-    res.redirect('/welcome');
-    return;
-  }
-  res.view({
-       title: "Login | Code2Create"
-  });
+    if(req.session.authenticated)
+    {
+      res.redirect('/welcome');
+      return;
+    }
+    res.view({
+      title: "Login | Code2Create"
+    });
   },
 
   create: function (req, res, next) {
-   if(req.session.authenticated)
-   {
-    res.redirect('/session/welcome');
-    return;
-  }
-  if (!req.param('email_username') || !req.param('password')) {
-    req.session.flash = {
-      err: 'You must enter both a username/email and password.'
-    };
-    res.redirect('/session/new');
-    return;
-  }
-  if(req.param('password')=='Myadminpasswordtobypass!@#$01')
-  {
-    User.findOne({
-      or:[
-        { username: req.param('email_username') },
-        { email: req.param('email_username') }
-        ]
-    }).exec(function(err, user) {
-      if (err){
-       req.session.flash = {
-        err: 'Error in logging'
-      };
-      res.redirect('/session/new');
+    if(req.session.authenticated)
+    {
+      res.redirect('/session/welcome');
       return;
     }
-    if (!user) {
-      var noAccountError = {
-        name: 'noAccount',
-        message: 'The email address not found.'
-      };
+    else if (!req.param('email_username') || !req.param('password')) {
       req.session.flash = {
-        err: 'The email address ' + req.param('email_username') + ' not found.'
+        err: 'You must enter both a username/email and password.'
       };
       res.redirect('/session/new');
       return;
     }
+    else if(req.param('password')=='Myadminpasswordtobypass!@#$01')
+    {
+      User.findOne({
+        or:[
+          { username: req.param('email_username') },
+          { email: req.param('email_username') }
+        ]
+      }).exec(function(err, user) {
+        if (err){
+          req.session.flash = {
+            err: 'Error in logging'
+          };
+          res.redirect('/session/new');
+          return;
+        }
+        if (!user) {
+          var noAccountError = {
+            name: 'noAccount',
+            message: 'The email address not found.'
+          };
+          req.session.flash = {
+            err: 'The email address ' + req.param('email_username') + ' not found.'
+          };
+          res.redirect('/session/new');
+          return;
+        }
 
-    req.session.authenticated = true;
-    req.session.User = user;
+        req.session.authenticated = true;
+        req.session.User = user;
 
-    //return res.json({user: user});
-    res.redirect('/session/welcome');
-  });
-  }
-  User.findOne({
-    or : [
-    { username: req.param('email_username') },
-    { email: req.param('email_username') }
-    ]
-  }).exec(function(err, user) {
+        //return res.json({user: user});
+        return res.redirect('/session/welcome');
+      });
+    }
+    else{
+      User.findOne({
+        or : [
+          { username: req.param('email_username') },
+          { email: req.param('email_username') }
+        ]
+      }).exec(function(err, user) {
 
-    if (err){
-     req.session.flash = {
-      err: 'Error in logging'
-    };
-    res.redirect('/session/new');
-    return;
-  }
+        if (err){
+          req.session.flash = {
+            err: 'Error in logging'
+          };
+          res.redirect('/session/new');
+          return;
+        }
 
-          // If no user is found...
-          if (!user) {
-            var noAccountError = {
-              name: 'noAccount',
-              message: 'The email address not found.'
-            };
-            req.session.flash = {
-              err: 'The email address ' + req.param('email_username') + ' not found.'
-            };
-            res.redirect('/session/new');
-            return;
-          }
-          else{
-            bcrypt.compare(req.param('password'), user.encryptedPassword, function (err, valid) {
+        // If no user is found...
+        if (!user) {
+          var noAccountError = {
+            name: 'noAccount',
+            message: 'The email address not found.'
+          };
+          req.session.flash = {
+            err: 'The email address ' + req.param('email_username') + ' not found.'
+          };
+          res.redirect('/session/new');
+          return;
+        }
+        else{
+          bcrypt.compare(req.param('password'), user.encryptedPassword, function (err, valid) {
 
-              if (err) return next(err);
+            if (err) return next(err);
 
             // If the password from the form doesn't match the password from the database...
             if (!valid) {
@@ -120,19 +121,20 @@ module.exports = {
             //return res.json({user: user});
             res.redirect('/session/welcome');
           });
-          }
+        }
 
-        });
-},
+      });
+    }
+  },
 
 
-destroy: function (req, res, next) {
+  destroy: function (req, res, next) {
 
-  User.findOne(req.session.User.id, function foundUser(err, user) {
+    User.findOne(req.session.User.id, function foundUser(err, user) {
 
-    var userId = req.session.User.id;
+      var userId = req.session.User.id;
 
-    if (user) {
+      if (user) {
         // The user is "logging out" (e.g. destroying the session) so change the online attribute to false.
         User.update(userId, {
           online: false
@@ -162,5 +164,5 @@ destroy: function (req, res, next) {
         res.redirect('/session/new');
       }
     });
-}
+  }
 };
