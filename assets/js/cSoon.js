b85f6854b1ace9f4e2258b52069e4df75697fa87
Madhav Bahl <madhavbahl10@gmail.com>
2017-12-22 08:44:09 +0530

    Change coming soon date

--- assets/js/cSoon.js
+++ assets/js/cSoon.js
@@ -56,7 +56,7 @@ $(window).load(function(){
 
 
 $('#countdown').countdown({
-	date: "December 20, 2017 00:00:00",
+	date: "January 01, 2018 00:00:00",
 	render: function(data) {
 	  var el = $(this.el);
 	  el.empty()
